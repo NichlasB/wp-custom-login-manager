c8b00e34e153b391d7cbe8130b3ac27b6f1820e9
NichlasB <apps@mailastic.com>
2026-03-12 19:26:13 +0100

    chore: update plugin author from CueFox to Alynt
    - Update Author field in plugin header to Alynt
    - Update copyright notice in translation template
    - Update author msgid in .pot file

--- wp-custom-login-manager.php
+++ wp-custom-login-manager.php
@@ -6,7 +6,7 @@
  * Version: 1.2.0
  * Requires at least: 5.0
  * Requires PHP: 7.2
- * Author: CueFox
+ * Author: Alynt
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-custom-login-manager
